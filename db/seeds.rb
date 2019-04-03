6e25a49ee59e4fe4f1a206e40ce27c800b8bd3f8
Luke Harris <lukeharris727@gmail.com>
2019-04-03 15:07:13 +0800

    Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,14 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+10.times do |restaurants|
+  Faker::Restaurant.name
+  Faker::Restaurant.address
+  Faker::Restaurant.category
+  Faker::Restaurant.phone_number
+  Faker::Restaurant.reviews
+
+end
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
